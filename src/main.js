5af27dab65d3ac79f43b0a453b4c94e9e07ec39d
Александр Шабалин <notabene1991@yandex.ru>
2020-09-06 17:54:00 +0300

    initial

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 import store from './store'
-=======
->>>>>>> 3e2e3f56b68b2b86bb9e5512659e0887c9c0ae6b
 
 Vue.config.productionTip = false
 
 new Vue({
-<<<<<<< HEAD
   store,
   render: h => h(App)
-=======
-  render: h => h(App),
->>>>>>> 3e2e3f56b68b2b86bb9e5512659e0887c9c0ae6b
 }).$mount('#app')
